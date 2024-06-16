2fcf2440ef323d83c9660869dccce3700c78193e
huyNTQ <huy.nguyenthacquang@employmenthero.com>
2024-06-16 12:38:04 +0700

    logging

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import { wait } from './wait'
+const https = require('https')
 
 /**
  * The main function for the action.
@@ -13,6 +14,17 @@ export async function run(): Promise<void> {
     core.debug(`Waiting ${ms} milliseconds ...`)
     console.log('HELLO WORLD!!!')
     core.info(`Hello, World!!!!`)
+
+    const token = core.getInput('TOKEN')
+    const latest_run_id = core.getInput('LATEST_RUN_ID')
+    console.log(`Hello, World!!!!`, token, latest_run_id)
+
+    // const artifactsResponse = await axios.get(`https://api.github.com/repos/${owner}/${repo}/actions/runs/${jobId}/artifacts`, {
+    //   headers: {
+    //     Authorization: `Bearer ${token}`,
+    //     Accept: 'application/vnd.github.v3+json'
+    //   }
+    // });
 
     // Log the current timestamp, wait, then log the new timestamp
     core.debug(new Date().toTimeString())
